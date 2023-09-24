5bf20ed1ee5880a47c52f347484d2f14076d7e23
VasiliiKuptsov <kuptsovy@mail.ru>
2023-09-24 20:45:06 +0300

    run

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -4,6 +4,33 @@ class DBManager:
 
     def __init__(self, db_user, db_password, db_host, db_port):
 
+        self.db_user = db_user
+        self.db_password = db_password
+        self.db_host = db_host
+        self.db_port = db_port)
+        self.connection = psycopg2.connect(db_name = 'postgres',
+                                            user = self.db_user,
+                                            password = self.db_password,
+                                            host = self.db_host,
+                                            port = self.db_port)
+
+
+    def create_database(self, db_name):
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('DROP DATABASE IF EXISTS {db_name}')
+        self.cursor.execute('CREATE DATABASE {db_name}')
+        self.cursor.commit()
+
+
+    def create_table(self):
+
+
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('CREATE TABLE employee ...;')
+        self.cursor.execute('CREATE TABLE vacancies ...;')
+        self.cursor.commit()
+
+
 
     """получает список всех компаний и количество вакансий у каждой компании. """
 
@@ -20,3 +47,6 @@ class DBManager:
 
     """ получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python."""
     def  get_vacancies_with_keyword():
+
+
+    def close_connection(self):
